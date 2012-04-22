d0c2282f66c2e3e4bb9c250f23dddeb74646d933
Nikola Metulev <nmetulev@uci.edu>
2012-04-22 00:27:33 -0700

    Added email validation

--- ContactUs.php
+++ ContactUs.php
@@ -1,14 +1,15 @@
 <html>
 <head>
+<script type="text/javascript" src="javascript/webdev.js"></script>
 <title>Contact Us</title>
 </head>
 <body>
-<form name="contact" action="sendmail.php" method="post">
-Your Name: <input type="text" name="name" /> <br />
-Your E-mail: <input type="text" name="email" /> <br />
-Subject: <input type="text" name="subject" /> <br />
+<form name="contact" action="sendmail.php" method="post" onsubmit="return validateForm()" >
+Your Name: <input type="text" name="name" id="name"/> <br />
+Your E-mail: <input type="text" name="email" id = "email"/> <br />
+Subject: <input type="text" name="subject" id="subject"/> <br />
 Content: <br />
-<textarea name="content" rows="20" cols="20" ></textarea><br />
+<textarea name="content" id="subject" rows="20" cols="20" ></textarea><br />
 
 <input type="submit" value="Submit" />
 </form>
