6eb7fd9de260ce9c45c5daa1918e627099318173
Nikola Metulev <nmetulev@uci.edu>
2012-04-21 23:16:07 -0700

    modified dbObject.php

--- dbObject.php
+++ dbObject.php
@@ -4,7 +4,7 @@ class dbObject {
     
     var $host = 'mysql.appjam.roboteater.com';
     var $username = 'icssc_workshop';
-    var $password = 'coolcoolcool';
+    var $password = 'macedonia';
     var $schema = 'webdev_workshop';
     
     /*
